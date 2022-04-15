cea116b797d1d983d60ee51904343ea1e1232a42
MaahesCr <psm3017@mail.ru>
2022-04-15 15:44:00 +0500

    v1

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,46 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+<div>
+    <button v-on:click="addLike">Like</button>
+    <button @click="addDislike">Dislike</button>
+</div>
+    <div>
+        <h3> Количество лайков {{likes}} </h3>
+        <h3> Количество  дизлайков {{dislikes}} </h3>
+    </div>
+    <div class="post">
+        <div> <strong>Название: </strong>Пост о JS</div>
+        <div> <strong>Описание: </strong>JS</div>
+    </div>
 </template>
+    
+<script>
+    export default {
+        data () {
+            return {
+                likes: 0, 
+                dislikes: 0,
+            }
+        }, 
+        methods: {
+            addLike() {
+                this.likes += 1
+            }, 
+            addDislike() {
+                this.dislikes += 1
+            }
+        }
+    }
+</script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+    * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+    .post {
+    padding: 15px;
+    border: 1px solid teal;
+    }
 </style>
